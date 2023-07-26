5bfdd1d1aaddfffc3e1ffca9b9c446f5da9ff6ca
santyago0 <santyago0515@gmail.com>
2023-07-26 17:29:43 -0500

    New Changues 04 - Clase Enlace

--- lenguaje-programacion/TelefoniaCelular/src/paquete04/Enlace.java
+++ lenguaje-programacion/TelefoniaCelular/src/paquete04/Enlace.java
@@ -6,88 +6,136 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import paquete02.*;
+import paquete03.*;
+import paquete04.*;
 
 public class Enlace {
     
     private Connection conn;
+    private ArrayList<PlanCelular> listaPlanes;
        
     public void establecerConexion() {  
 
-        try {  
-            // db parameters  
-            String url = "jdbc:sqlite:bd/trabajadores.bd";  
-            // create a connection to the database  
-            conn = DriverManager.getConnection(url);  
-            // System.out.println(conn.isClosed());
-            // System.out.println("Connection to SQLite has been established.");  
+        try {
+            String url = "jdbc:sqlite:bd/base01.bd";
+            conn = DriverManager.getConnection(url);
               
         } catch (SQLException e) {  
             System.out.println(e.getMessage());  
         }   
         
-    } 
+    }
     
     public Connection obtenerConexion(){
         return conn;
     }
     
-    public void insertarTrabajador(Trabajador worker) {  
+    public void insertarPlanPostPagoMinutosMegasEconomico
+        (PlanPostPagoMinutosMegasEconomico p) {  
   
         try{  
             establecerConexion();
             Statement statement = obtenerConexion().createStatement();
-            String data = String.format("INSERT INTO Trabajador (nombre, "
-                    + "cedula, correo, mesSueldo, sueldo) "
-                    + "values ('%s','%s','%s','%s','%.2f')", 
-                    worker.obtenerNombre(),
-                    worker.obtenerCedula(),
-                    worker.obtenerCorreo(),
-                    worker.obtenerMesSuelo(),
-                    worker.obtenerSueldo());
-            System.out.println(data);
+            String data = String.format("INSERT INTO PlanPostPagoMinutosMegasEconomico "
+                    + "(nombreCompleto, pasaporte, ciudad, barrio, marca, modelo, "
+                    + "celular, minutos, costoMin, megasGB, costoGB, porcentajeDescuento) "
+                    + "values ('%s', '%s', '%s', '%s', '%s', '%s', '%s', %d, '%.2f',"
+                    + "'%.2f', '%.2f', '%.2f')", 
+                    p.obtenerPropietario().obtenerNombreCompleto(), 
+                    p.obtenerPropietario().obtenerPasaporte(),
+                    p.obtenerPropietario().obtenerCiudad(),
+                    p.obtenerPropietario().obtenerBarrio(),
+                    p.obtenerCelular().obtenerMarca(),
+                    p.obtenerCelular().obtenerModelo(),
+                    p.obtenerCelular().obtenerCelular(),
+                    p.obtenerMinutos(),
+                    p.obtenerCostoMin(),
+                    p.obtenerMegasGB(),
+                    p.obtenerCostoGB(),
+                    p.obtenerPorcentajeDescuento());
+            
             statement.executeUpdate(data);
             obtenerConexion().close();
             
         } catch (SQLException e) {  
+             System.out.println("Exception: PlanPostPagoMinutosMegasEconomico");
+             System.out.println(e.getMessage());  
+             
+        }  
+    }
+        
+    public void insertarPlanPostPagoMinutosMegas (PlanPostPagoMinutosMegas p) {  
+  
+        try{  
+            establecerConexion();
+            Statement statement = obtenerConexion().createStatement();
+            String data = String.format("INSERT INTO PlanPostPagoMinutosMegas "
+                    + "(nombreCompleto, pasaporte, ciudad, barrio, marca, modelo,"
+                    + " celular, minutos, costoMin, megasGB, costoGB) values "
+                    + "('%s', '%s', '%s', '%s', '%s', '%s', '%s', %d, '%.2f',"
+                    + "'%.2f', '%.2f')", 
+                    p.obtenerPropietario().obtenerNombreCompleto(), 
+                    p.obtenerPropietario().obtenerPasaporte(),
+                    p.obtenerPropietario().obtenerCiudad(),
+                    p.obtenerPropietario().obtenerBarrio(),
+                    p.obtenerCelular().obtenerMarca(),
+                    p.obtenerCelular().obtenerModelo(),
+                    p.obtenerCelular().obtenerCelular(),
+                    p.obtenerMinutos(),
+                    p.obtenerCostoMin(),
+                    p.obtenerMegasGB(),
+                    p.obtenerCostoGB());
             
-             System.out.println("Exception:");
+            statement.executeUpdate(data);
+            obtenerConexion().close();
+            
+        } catch (SQLException e) {  
+             System.out.println("Exception: PlanPostPagoMinutosMegas");
              System.out.println(e.getMessage());  
              
         }  
     }
     
-    public ArrayList<Trabajador> obtenerDataTrabajador() { 
-        
-        ArrayList<Trabajador> lista = new ArrayList<>();
+    public void establecerlistaPlanes() {  
+        listaPlanes = new ArrayList<>();
         
         try{  
-            
             establecerConexion();
             Statement statement = obtenerConexion().createStatement();
-            String data = "Select * from Trabajador;";
+            String data = "Select * from PlanPostPagoMinutosMegasEconomico;";
             
             ResultSet rs = statement.executeQuery(data);
             while(rs.next()){
+                Propietario p = new Propietario(rs.getString("nombreCompleto"),
+                rs.getString("pasaporte"),rs.getString("ciudad"),
+                        rs.getString("barrio"));
                 
-                Trabajador t = new Trabajador(rs.getString("nombre"),
-                        rs.getString("cedula"),
-                        rs.getString("correo"),
-                        rs.getString("mesSueldo"),
-                        rs.getDouble("sueldo"));
+                Celular c = new Celular(rs.getString("marca"),
+                        rs.getString("modelo"),rs.getString("celular"));
                 
-                lista.add(t);
+                PlanPostPagoMinutosMegasEconomico pC = 
+                        new PlanPostPagoMinutosMegasEconomico(p,c,
+                                rs.getInt("minutos"),
+                               rs.getDouble("costoMin"),
+                               rs.getDouble("megasGB"),
+                               rs.getDouble("costoGB"),
+                               rs.getDouble("porcentajeDescuento"));
                 
+                listaPlanes.add(pC);
             }
             
             obtenerConexion().close();
             
-        } catch (SQLException e) { 
-            
-             System.out.println("Exception: insertarTrabajador");
+        } catch (SQLException e) {  
+             System.out.println("Exception: insertarPlanCelular");
              System.out.println(e.getMessage());  
              
-        }  
-        return lista;
+        }
+    }
+    
+    public ArrayList<PlanCelular> obtenerlistaPlanes() {
+        return listaPlanes;
     }
     
 }
