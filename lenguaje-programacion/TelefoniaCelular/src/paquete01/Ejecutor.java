d457ac7f09d6e6f9b35dedd1214bce4c0ecbe56b
JOSEPHMATHEO <91102368+JOSEPHMATHEO@users.noreply.github.com>
2023-07-26 20:57:15 -0500

    New Changes 08 - Creacion Menu

--- lenguaje-programacion/TelefoniaCelular/src/paquete01/Ejecutor.java
+++ lenguaje-programacion/TelefoniaCelular/src/paquete01/Ejecutor.java
@@ -1,5 +1,6 @@
 package paquete01;
 
+import java.util.Scanner;
 import paquete04.*;
 import paquete02.*;
 import paquete03.*;
@@ -10,6 +11,126 @@ public class Ejecutor {
 
         Enlace e = new Enlace();
         
+        System.out.println("========================================");
+        System.out.println("Sistema de Gestio de Planes de Telefonia");
+        System.out.println("========================================\n");
+
+        // Varaibles y Objetos Globales
+
+        int opcion;
+        String nombre;
+        String pasaporte;
+        String ciudad;
+        String barrio;
+
+        Scanner lm = new Scanner(System.in);
+
+        boolean condicion = true;
+
+        // Creacion del menu del programa
+        
+        while (condicion) {
+
+            System.out.printf("%s\n\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n\n%s ",
+                    "--Menu de Opciones para Planes de Telefonia--",
+                    "1) Plan Post Pago Megas",
+                    "2) Plan Post Pago Minutos",
+                    "3) Plan Post Pago Minutos Megas",
+                    "4) Plan Post Pago Minutos Megas Econocmico",
+                    "5) Salir del Programa",
+                    "Elija una opcion:");
+
+            opcion = lm.nextInt();
+
+           switch (opcion) {
+
+                case 1:
+                    
+                    System.out.print("Ingrese el nombre completo del Cliente: ");
+                    nombre = lm.nextLine();
+                    
+                    System.out.print("Ingrese el pasaporte del Cliente: ");
+                    pasaporte = lm.nextLine();
+                    
+                    System.out.print("Ingrese la ciudad del Cliente: ");
+                    ciudad = lm.nextLine();
+                    
+                    System.out.print("Ingrese el barrio del Cliente: ");
+                    barrio = lm.nextLine();
+                    
+                    Propietario p = new Propietario(nombre, pasaporte, ciudad, barrio);
+                    
+                    break;
+                    
+                case 2:
+                    
+                    System.out.print("Ingrese el nombre completo del Cliente: ");
+                    nombre = lm.nextLine();
+                    
+                    System.out.print("Ingrese el pasaporte del Cliente: ");
+                    pasaporte = lm.nextLine();
+                    
+                    System.out.print("Ingrese la ciudad del Cliente: ");
+                    ciudad = lm.nextLine();
+                    
+                    System.out.print("Ingrese el barrio del Cliente: ");
+                    barrio = lm.nextLine();
+                    
+                    Propietario p2 = new Propietario(nombre, pasaporte, ciudad, barrio);
+                    
+                    break;
+                    
+                case 3: 
+                    
+                    System.out.print("Ingrese el nombre completo del Cliente: ");
+                    nombre = lm.nextLine();
+                    
+                    System.out.print("Ingrese el pasaporte del Cliente: ");
+                    pasaporte = lm.nextLine();
+                    
+                    System.out.print("Ingrese la ciudad del Cliente: ");
+                    ciudad = lm.nextLine();
+                    
+                    System.out.print("Ingrese el barrio del Cliente: ");
+                    barrio = lm.nextLine();
+                    
+                    Propietario p3 = new Propietario(nombre, pasaporte, ciudad, barrio);
+                    
+                    break;
+                    
+                case 4:
+                    
+                    System.out.print("Ingrese el nombre completo del Cliente: ");
+                    nombre = lm.nextLine();
+                    
+                    System.out.print("Ingrese el pasaporte del Cliente: ");
+                    pasaporte = lm.nextLine();
+                    
+                    System.out.print("Ingrese la ciudad del Cliente: ");
+                    ciudad = lm.nextLine();
+                    
+                    System.out.print("Ingrese el barrio del Cliente: ");
+                    barrio = lm.nextLine();
+                    
+                    Propietario p4 = new Propietario(nombre, pasaporte, ciudad, barrio);
+                    
+                    break;  
+                    
+                case 5:
+                    
+                    System.err.println("Saliendo del Programa...");
+                    condicion = false;
+
+                    break;
+                    
+                default:
+                    
+                    System.out.println("\nOpcion incorrecta...");
+                    
+           }
+           
+        }
+                    
         Propietario p = new Propietario("Luis miguel Morales Bautista", "ZAB000254", "Loja", "El Plateado");
         
         Celular c = new Celular("Samsung", "A21S", "0991526978");
